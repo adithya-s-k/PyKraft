fcc6766fbbc7cf7eff298d4fa257c4a3e0d84c92
Adithya S K <adithyaskolavi@gmail.com>
2024-03-14 00:29:44 +0530

    feat(utils): add utilities in utils.py - Adithya S K

---
+++ raft/utils.py
@@ -0,0 +1,44 @@
+import json
+
+
+def load_data(path):
+    try:
+        with open(path, 'r') as file:
+            data = json.load(file)
+            return data
+    except:
+        data =  {
+            "RegisterBrokerRecords": {
+                "records": [],
+                "timestamp": ""
+            },
+            "TopicRecord": {
+                "records": [],
+                "timestamp": ""
+            },
+            "PartitionRecord": {
+                "records": [],
+                "timestamp": ""
+            },
+            "ProducerIdsRecord": {
+                "records": [],
+                "timestamp": ""
+            },
+            "RegistrationChangeBrokerRecord": {
+                "records": [],
+                "timestamp": ""
+            }
+        }
+        return data
+
+def save_data(path,data):
+    with open(path, 'w') as file:
+        json.dump(data, file, indent=2)
+
+def checkBrokerExists(brokerData,data):
+    found_dict = next((broker for broker in data if broker.get("brokerId") == brokerData['brokerId']), None)
+    return found_dict
+
+def checkProducerExists(brokerData,data):
+    found_dict = next((broker for broker in data if broker.get("producerId") == brokerData['producerId'] and broker.get("brokerId")==brokerData["brokerId"]), None)
+    return found_dict
